84cfb38fa15c69abc65fb8432b9bda6c0015a73a
sujin2f <sujin2f@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 09:22:04 +0000

    = 1.0 = * Original Version
    git-svn-id: https://plugins.svn.wordpress.org/evanesco/trunk@656751 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ evanesco.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Evanesco!
+ * Plugin URI: http://www.sujinc.com/lab/evanesco/
+ * Description: You can hide or show Wordpress menu and widget.
+ * Version: 1.0
+ * Author: Sujin 수진 Choi 최
+ * Author URI: http://www.sujinc.com/
+ * License: GPLv2 or later
+ * Text Domain: evanesco
+ */
+
+require_once('functions.php');
